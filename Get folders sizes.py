606df02175bd8c0a6d6adf59bd5e1292b0f9b08f
ZolotarevAlexandr <Zolotarev10032006@mail.ru>
2023-01-22 15:00:10 +0300

    Initial commit

---
+++ Get folders sizes.py
@@ -0,0 +1,49 @@
+import os
+
+
+def human_read_format(size):
+    sizes_types = ['Б', 'КБ', 'МБ', 'ГБ']
+    counter = 0
+
+    while size >= 1024:
+        size = size / 1024
+        counter += 1
+
+    return round(size, 2), sizes_types[counter]
+
+
+def get_folder_size(name):
+    folder_size = 0
+    for path, dirs, files in os.walk(name):
+        for f in files:
+            fp = os.path.join(path, f)
+            folder_size += os.path.getsize(fp)
+    return folder_size
+
+
+def main():
+    sizes_types = ['Б', 'КБ', 'МБ', 'ГБ']
+
+    result = []
+    all_dirs_len = len(os.listdir())
+    for index, item in enumerate(os.listdir()):
+        try:
+            if os.path.isfile(item):
+                result.append((item, human_read_format(os.path.getsize(item))))
+            else:
+                result.append((item, human_read_format(get_folder_size(item))))
+        except Exception as e:
+            print(f'Exception ({e}) occurred on file {item}')
+        print(f'Progress: {round((index / all_dirs_len) * 100)}%')
+
+    result = sorted(result, key=lambda x: (sizes_types.index(x[1][1]), x[1][0]), reverse=True)
+    for i, item in enumerate(result):
+        print(f'{item[0]} - {item[1][0]}{item[1][1]}')
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception as e:
+        print(f'Exception ({e}) occurred')
+    input('Press any button to exit')
